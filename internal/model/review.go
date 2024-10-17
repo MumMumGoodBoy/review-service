bfc9b904eb591496472e94ea167fccaa030ab5e1
Teetat Wisanuyotin <teetat@Teetats-MacBook-Pro.local>
2024-10-18 00:55:34 +0700

    fix: change type of user_id from string to int

--- internal/model/review.go
+++ internal/model/review.go
@@ -5,9 +5,18 @@ import (
 )
 
 type Review struct {
-  gorm.Model
-  RestaurantId string
-  UserId string
-  Rating float32
-  Content string
+	gorm.Model
+	RestaurantId string
+	UserId       int32
+	Rating       float32
+	Content      string
+}
+
+type ReviewEvent struct {
+	Event        string  `json:"event"`
+	Id           int     `json:"id"`
+	ReviewerId   int     `json:"reviewer_id"`
+	RestaurantId string  `json:"restaurant_id"`
+	Rating       float32 `json:"rating"`
+	Content      string  `json:"content"`
 }
