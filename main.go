b0321c8ca4b265395ae9bbcca80b4d1655795560
TeerapatChan <teerapatchantaramanee@gmail.com>
2024-10-20 08:56:57 +0700

    fix: port, compose

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"os"
@@ -20,12 +19,15 @@ func main() {
 	if err := godotenv.Load(".env"); err != nil {
 		log.Fatalf("Error loading .env file")
 	}
+	postgresUrl := os.Getenv("POSTGRES_URL")
+	port := os.Getenv("PORT")
 
-	db, err := gorm.Open(postgres.Open("host=localhost user=user password=pass dbname=review port=5432"), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(postgresUrl), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
+	log.Println("Connected to database")
 	db.AutoMigrate(&model.Review{})
 	db.AutoMigrate(&model.FavoriteFood{})
 
@@ -40,8 +42,8 @@ func main() {
 		log.Fatal(err)
 	}
 	defer rabbitMQChannel.Close()
-	fmt.Println("Connected to RabbitMQ")
 
+	log.Println("Connected to RabbitMQ")
 	// service
 	reviewService := service.ReviewService{
 		DB:              db,
@@ -52,11 +54,12 @@ func main() {
 	grpcServer := grpc.NewServer()
 	proto.RegisterReviewServer(grpcServer, &reviewService)
 
-	lis, err := net.Listen("tcp", ":9001")
+	lis, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	log.Println("Review service is running on port", port)
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatal(err)
 	}
